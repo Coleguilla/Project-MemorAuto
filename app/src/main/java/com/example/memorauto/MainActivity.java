c3475916c52ce0ad07f7e77410a04bf093b4178d
JDavid <josedavidne@hotmail.com>
2024-05-21 22:53:56 +0200

    Cuarto commit

--- app/src/main/java/com/example/memorauto/MainActivity.java
+++ app/src/main/java/com/example/memorauto/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.memorauto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -26,13 +29,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
         configView();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_principal, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     private void configView() {
@@ -41,6 +47,15 @@ public class MainActivity extends AppCompatActivity {
         etModelo = findViewById(R.id.etModelo);
         tvFecha = findViewById(R.id.tvFecha);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_favorite) {
+            Log.d("PRUEBA", "funciona" );
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void guardar(View view) {
